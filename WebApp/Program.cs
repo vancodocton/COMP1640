c339076aa89edcc0127ebd53d32dd747ac388275
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-02-25 12:59:15 +0700

    add Areas Admin for system administrator

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -36,9 +36,15 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+           name: "areas",
+           pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+});
 app.MapRazorPages();
 
 app.Run();
