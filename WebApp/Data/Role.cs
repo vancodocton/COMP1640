01fb82914a1c4e630ea3437b2a7e06faf64d81f1
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-02-25 23:10:23 +0700

    Add class Role for access role name convenience

---
+++ WebApp/Data/Role.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class Role
+    {
+        public const string Admin = "admin";
+
+        public const string Manager = "manager";
+
+        public const string Coordinator = "coordinator";
+
+        public const string Staff = "staff";
+    }
+}
