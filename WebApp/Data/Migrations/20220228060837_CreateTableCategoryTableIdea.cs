dd6f516e1981a6b1be1abe87c639433b6b752cfe
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-02-28 13:12:44 +0700

    design table idea and category; add relationships; migrate to existing database.

---
+++ WebApp/Data/Migrations/20220228060837_CreateTableCategoryTableIdea.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    public partial class CreateTableCategoryTableIdea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Idea",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Idea", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Idea_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Idea_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Idea_CategoryId",
+                table: "Idea",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Idea_UserId",
+                table: "Idea",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Idea");
+        }
+    }
+}
---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public IEnumerable<Idea>? Ideas { get; set; }
+    }
+}
