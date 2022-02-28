55511b11a572a4aed71cebc7790fd8d266401365
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-02-28 20:28:44 +0700

    create IdeaViewModel

---
+++ WebApp/ViewModels/IdeaViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ViewModels
+{
+    public class IdeaViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string? Content { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [ValidateNever]
+        public List<SelectListItem>? Categories { get; set; }
+    }
+}
