e04cc04a9f562b4c03fd9b2dc3d16b87e11ce952
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-02-28 11:00:20 +0700

    seed sample data to department table

--- WebApp/Data/SeedData.cs
+++ WebApp/Data/SeedData.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Data
@@ -7,61 +8,79 @@ namespace WebApp.Data
     {
         public async static Task Initializer(IServiceProvider serviceProvider, IConfiguration configuration)
         {
-            using (var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>())
+            await SeedRoleData(serviceProvider.GetRequiredService<RoleManager<IdentityRole>>());
+
+            await SeedDefaultAccountData(serviceProvider.GetRequiredService<UserManager<ApplicationUser>>());
+
+            await SeedDepartmentData(serviceProvider.GetRequiredService<ApplicationDbContext>());
+        }
+
+        private static async Task SeedRoleData(RoleManager<IdentityRole> roleManager)
+        {
+            var roles = new List<string>()
             {
-                if (await roleManager.RoleExistsAsync(Role.Admin) == false)
-                {
-                    await roleManager.CreateAsync(new IdentityRole(Role.Admin));
-                }
+                Role.Admin, Role.Manager, Role.Coordinator, Role.Staff
+            };
 
-                if (await roleManager.RoleExistsAsync(Role.Manager) == false)
+            foreach (var role in roles)
+            {
+                if (await roleManager.RoleExistsAsync(role) == false)
                 {
-                    await roleManager.CreateAsync(new IdentityRole(Role.Manager));
-                }
-
-                if (await roleManager.RoleExistsAsync(Role.Coordinator) == false)
-                {
-                    await roleManager.CreateAsync(new IdentityRole(Role.Coordinator));
-                }
-
-                if (await roleManager.RoleExistsAsync(Role.Staff) == false)
-                {
-                    await roleManager.CreateAsync(new IdentityRole(Role.Staff));
+                    await roleManager.CreateAsync(new IdentityRole(role));
                 }
             }
-            using (var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>())
+        }
+
+        private static async Task SeedDefaultAccountData(UserManager<ApplicationUser> userManager)
+        {
+            var admin = new ApplicationUser
             {
-                var admin = new ApplicationUser
-                {
-                    UserName = "admin@g.c",
-                    Email = "admin@g.c",
-                    EmailConfirmed = true,
-                };
-                var manager = new ApplicationUser
-                {
-                    UserName = "manager@g.c",
-                    Email = "manager@g.c",
-                    EmailConfirmed = true,
-                };
+                UserName = "admin@g.c",
+                Email = "admin@g.c",
+                EmailConfirmed = true,
+            };
+            var manager = new ApplicationUser
+            {
+                UserName = "manager@g.c",
+                Email = "manager@g.c",
+                EmailConfirmed = true,
+            };
+            string password = "asd@12E";
 
-                string password = "asd@12E";
+            // Seed admin account
+            if (await userManager.FindByEmailAsync(admin.Email) == null)
+            {
+                var result = await userManager.CreateAsync(admin, password);
 
-                if (await userManager.FindByEmailAsync(admin.Email) == null)
-                {
-                    var result = await userManager.CreateAsync(admin, password);
-
-                    if (result.Succeeded)
-                        await userManager.AddToRoleAsync(admin, Role.Admin);
-                }
-
-                if (await userManager.FindByEmailAsync(manager.Email) == null)
-                {
-                    var result = await userManager.CreateAsync(manager, password);
-
-                    if (result.Succeeded)
-                        await userManager.AddToRoleAsync(manager, Role.Manager);
-                }
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(admin, Role.Admin);
             }
+
+            // Seed manager account
+            if (await userManager.FindByEmailAsync(manager.Email) == null)
+            {
+                var result = await userManager.CreateAsync(manager, password);
+
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(manager, Role.Manager);
+            }
+        }
+
+        private static async Task SeedDepartmentData(ApplicationDbContext context)
+        {
+            var departments = new List<Department>()
+            {
+                new Department() { Name="Academic" },
+                new Department() { Name="Support" },
+            };
+
+            foreach (var department in departments)
+            {
+                if (!await context.Department.AnyAsync(d => d.Name == department.Name))
+                    await context.Department.AddAsync(department);
+            }
+
+            await context.SaveChangesAsync();
         }
     }
 }
