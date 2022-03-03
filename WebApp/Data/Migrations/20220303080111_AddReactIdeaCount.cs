8154916a6ddf5fcc3692b250f7cb56d56b848ef0
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-03-03 15:02:14 +0700

    add react count properties for idea table

---
+++ WebApp/Data/Migrations/20220303080111_AddReactIdeaCount.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    public partial class AddReactIdeaCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ThumbDown",
+                table: "Idea",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ThumbUp",
+                table: "Idea",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbDown",
+                table: "Idea");
+
+            migrationBuilder.DropColumn(
+                name: "ThumbUp",
+                table: "Idea");
+        }
+    }
+}
