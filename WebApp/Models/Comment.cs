57fc5deaad04b9f41e11846545258260365ab675
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-03-07 17:40:19 +0700

    add comment table and database migration

---
+++ WebApp/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? UserId { get; set; }
+        public ApplicationUser? User { get; set; }
+
+        /*
+         * In fact IdeaId is required but if it is set to be reqired, 
+         * it may cause cycles or mutiple cascade paths. 
+         */
+        public int? IdeaId { get; set; }
+        public Idea? Idea { get; set; }
+
+        [Required]
+        public string? Content { get; set; }
+    }
+}
