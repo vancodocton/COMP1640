69407bef71fb51ec0502c5cef4c908d7ae2bc7ec
Thien-Pham25 <pdthienvn@gmail.com>
2022-03-11 09:35:43 +0700

    add roles for categories

--- WebApp/Controllers/CategoriesController.cs
+++ WebApp/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
+    [Authorize(Roles = Role.Coordinator)]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
