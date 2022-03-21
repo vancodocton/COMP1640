0cba80b0ebb7a544d2277dd8e79014e212820df1
HoangXuanLoc <hoanglocth@gmail.com>
2022-03-21 16:33:36 +0700

    add statisticalcontroller to view user in own department and send email encourage

---
+++ WebApp/Areas/Coordinator/Controllers/StatisticalController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Areas.Coordinator.Controllers
+{
+    [Area("Coordinator")]
+    [Authorize(Roles = Role.Coordinator)]
+    public class StatisticalController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IEmailSender sender;
+
+        public StatisticalController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            IEmailSender sender
+            )
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.sender = sender;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var user = await userManager.GetUserAsync(HttpContext.User);
+            var idea = await context.Idea.FirstOrDefaultAsync(i => i.Id == 1);
+            var coors = user.DepartmentId;
+            var model = await userManager.Users.Where(u => u.DepartmentId == coors)
+                .Select(u => new StatisticalAnalysisViewModel()
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                })
+                .ToListAsync();
+            return View(model);
+        }
+    }
+}
