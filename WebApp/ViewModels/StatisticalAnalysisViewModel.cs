18b80d1cf0e68e77fd003c9c22a3c91c4685ec0a
HoangXuanLoc <hoanglocth@gmail.com>
2022-03-21 16:33:36 +0700

    add statisticalcontroller to view user in own department and send email encourage

---
+++ WebApp/ViewModels/StatisticalAnalysisViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class StatisticalAnalysisViewModel
+    {
+        public string? Id { get; set; }
+
+        public string? Email { get; set; }
+        public ApplicationUser? User { get; set; }
+    }
+}
