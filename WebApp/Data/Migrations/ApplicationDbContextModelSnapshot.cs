8b4dfcdf36138bd53403e6a2ea617f0e25585c44
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-04-05 16:58:07 +0700

    Update package

--- WebApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ WebApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace WebApp.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.2")
+                .HasAnnotation("ProductVersion", "6.0.3")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
