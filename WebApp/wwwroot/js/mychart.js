9ea2ee3aa982e38682cc3d1aa495237c67e2d3e2
HoangXuanLoc <hoanglocth@gmail.com>
2022-04-14 22:36:58 +0700

    fix ignored file not committed

---
+++ WebApp/wwwroot/js/mychart.js
@@ -0,0 +1,64 @@
+﻿const labels = [
+    'Academic Department',
+    'Support Department'];
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Idea',
+            data: [10, 20, 30],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'Comment',
+            data: [30, 20, 40],
+            backgroundColor: [
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 159, 64)'
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'React',
+            data: [30, 20, 40],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
+                'rgb(75, 192, 192)'
+            ],
+            borderWidth: 1
+        }]
+};
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+/*        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }*/
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Contribution of each Department'
+            }
+        }
+    },
+};
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config);
---
+++ WebApp/wwwroot/js/piechart.js
@@ -0,0 +1,32 @@
+﻿////const labels = [
+////    'Academic Department',
+////    'Support Department'];
+const data = {
+    labels: ['Orange', 'Blue'],
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [10, 20],
+            backgroundColor: 'rgb(255, 99, 132)'
+        }
+    ]
+};
+const config = {
+    type: 'pie',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Pie Chart'
+            }
+        }
+    },
+};
+const pieChart = new Chart(
+    document.getElementById('pieChart'),
+    config);
