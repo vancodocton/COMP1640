229486925981cff862d65c0281bce5c67e300f28
Nguyễn Kỳ Dương Trường <duongtruong.nguyenky@gmail.com>
2022-04-15 08:27:12 +0700

    Mapping data to bar chart

--- WebApp/wwwroot/js/mychart.js
+++ WebApp/wwwroot/js/mychart.js
@@ -1,63 +1,61 @@
-﻿const labels = [
-    'Academic Department',
-    'Support Department'];
-const data = {
-    labels: labels,
-    datasets: [
-        {
-            label: 'Idea',
-            data: [10, 20, 30],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgb(255, 99, 132)'
-            ],
-            borderWidth: 1
-        },
-        {
-            label: 'Comment',
-            data: [30, 20, 40],
-            backgroundColor: [
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgb(255, 159, 64)'
-            ],
-            borderWidth: 1
-        },
-        {
-            label: 'React',
-            data: [30, 20, 40],
-            backgroundColor: [
-                'rgba(75, 192, 192, 0.2)',
-            ],
-            borderColor: [
-                'rgb(75, 192, 192)'
-            ],
-            borderWidth: 1
-        }]
-};
-const config = {
+﻿const config = {
     type: 'bar',
-    data: data,
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                position: 'top',
+    data: {
+        labels: ['Academic', 'Support'],
+        datasets: [
+            {
+                label: 'React',
+                data: datasets,
+                parsing: {
+                    xAxisKey: 'label',
+                    yAxisKey: 'countIdea'
+                },
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)'
+                ],
+                borderWidth: 1
             },
-            title: {
-                display: true,
-                text: 'Contribution of each Department'
+            {
+                label: 'Comment',
+                data: datasets,
+                parsing: {
+                    xAxisKey: 'label',
+                    yAxisKey: 'countComment'
+                },
+                data: [30, 20, 40],
+                backgroundColor: [
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 159, 64)'
+                ],
+                borderWidth: 1
+            },
+            {
+                label: 'React',
+                data: datasets,
+                parsing: {
+                    xAxisKey: 'label',
+                    yAxisKey: 'countReact'
+                },
+                data: [30, 20, 40],
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgb(75, 192, 192)'
+                ],
+                borderWidth: 1
             }
-        }
+        ]
     },
 };
 var ctx = document.getElementById("myChart").getContext('2d');
 ctx.width = 100;
 ctx.height = 100;
-var my = new Chart(ctx, 
+var my = new Chart(ctx,
     config
 );
